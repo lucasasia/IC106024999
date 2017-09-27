75cb6ad4dc1dd639d967d86a9f5ca1832eadf690
lucasasia <lucas@asia.edu.tw>
2017-09-27 20:09:49 +0800

    modify my code again

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("106024999 Lu");
+        System.out.println("Ching-Ta Lu!");
+        System.out.println("Asia University!");// write your code here
+    }
+}
